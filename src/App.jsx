e38e17350c735682dde66e88aeb4a96de563566b
krivar <ivar@kristvik-contractors.no>
2023-09-08 20:05:31 +0200

    Modified, but not working .... seem to get lost in })};

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     // await keyword is used with Promise returning functions to tell JS to wait until the Promise is fulfilled
     // "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=DP01,DP05,DP10,DSND,DSNW,DT00,DT32,DX32,DX70,DX90,SNOW,PRCP&stations=ASN00084027&startDate=1952-01-01&endDate=1970-12-31&includeAttributes=true&format=json"
     const response = await fetch(
-      "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=DP01,DP05,DP10,DSND,DSNW,DT00,DT32,DX32,DX70,DX90,SNOW,PRCP&stations=ASN00084027&startDate=1952-01-01&endDate=1970-12-31&includeAttributes=true&format=json"
+      "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=PRCP&stations=ASN00084027&startDate=1952-01-01&endDate=1980-12-31&includeAttributes=true&format=json"
     );
     // .json() is a method built into the return value of fetch, it grabs the JSON data from the response.
     const converted = await response.json();
@@ -34,9 +34,16 @@ function App() {
   return (
     <div className="App">
       <h1>Fetch, Async & useEffect</h1>
-      {data && <p>{data}</p>}
+      {data && <p>{data.map((data, key ) => 
+          {return ( <div key={key}> data.DATE + 
+          " , " + 
+          data.PRCP + "<br/>"
+          </div>
+          ); 
+        })}
+      }
       <button onClick={grabData}>Fetch Data</button>
-    </div>
+    
   );
 }
 
